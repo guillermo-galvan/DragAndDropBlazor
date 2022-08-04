a4000dc22faf8fabce37c0240f34a568ed2a2a14
guillermo-galvan <101075067+guillermo-galvan@users.noreply.github.com>
2022-08-03 20:41:35 -0500

    Add project files.

---
+++ src/DragAndDrop/Models/DragDropModel.cs
@@ -0,0 +1,18 @@
+﻿namespace DragAndDrop.Models
+{
+    public class DragDropModel<T>
+    {
+        public int Index { get; set; }
+
+        public T? Item { get; set; }
+
+        public int NewIndex { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public override string ToString()
+        {
+            return $"Index: {Index} IndexNew: {NewIndex}";
+        }
+    }
+}
---
+++ src/DragAndDrop/Shared/DragDrop.razor.cs
@@ -0,0 +1,89 @@
+﻿using DragAndDrop.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace DragAndDrop.Shared
+{
+    public partial class DragDrop<TItem>
+    {
+        [Parameter]
+        public IEnumerable<TItem>? Items { get; set; }
+
+        [Parameter]
+        public RenderFragment<TItem>? ItemRender { get; set; }
+
+        readonly List<DragDropModel<TItem>> _newPosition = new();
+        DragDropModel<TItem>? _selected;
+        protected override void OnParametersSet()
+        {
+            if (Items != null)
+            {
+                _newPosition.Clear();
+                int count = 1;
+                foreach (var item in Items)
+                {
+                    _newPosition.Add(new DragDropModel<TItem> { Index = count++, Item = item });
+                }
+            }
+        }
+
+        void OnClick(DragDropModel<TItem> item)
+        {
+            _newPosition.FindAll(x => x != item).ForEach(x => x.IsActive = false);
+            item.IsActive = !item.IsActive;
+        }
+
+        void OnDragStart(DragDropModel<TItem> item)
+        {
+            _newPosition.ForEach(x => x.IsActive = false);
+            _selected = item;
+        }
+
+        void OnDragLeave(DragDropModel<TItem> item)
+        {
+
+        }
+
+        void OnDragEnter(DragDropModel<TItem> item)
+        {
+            if (_selected != null)
+            {
+                _selected.NewIndex = item.NewIndex > 0 ? item.NewIndex : item.Index;
+                _newPosition.Remove(_selected);
+                _newPosition.Insert(_selected.NewIndex - 1, _selected);
+            }
+
+            int count = 1;
+            _newPosition.ForEach(x => x.NewIndex = count++);
+        }
+
+        void OnDragEnd()
+        {
+            if (_selected != null)
+            {
+                _selected.IsActive = true;
+                _newPosition.Remove(_selected);
+                _newPosition.Insert(_selected.NewIndex - 1, _selected);
+                GenerateNewIndex();
+                OnClick(_selected);
+                _selected.IsActive = true;
+                _selected = null;
+            }
+        }
+
+        void GenerateNewIndex()
+        {
+            int count = 1;
+            _newPosition.ForEach(x => { x.Index = count++; x.NewIndex = 0; });
+        }
+
+        string CheckIfDragOperationIsInProgess()
+        {
+            return _selected == null ? "" : "dd-inprogess";
+        }
+
+        string GetClassForEnterDrag(DragDropModel<TItem> item)
+        {
+            return _selected != item || _selected == null ? "" : "dd-enter active";
+        }
+    }
+}
